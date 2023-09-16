acad1c22bfdca85333e5e5c9c6b77a2a9f592e7f
Amelia Helland <amelia@sigmacomputing.com>
2023-09-16 19:59:16 +0000

    Add product snapshot.

---
+++ greenery/snapshots/products_snapshot.sql
@@ -0,0 +1,20 @@
+{% snapshot products_snapshot %}
+
+{{
+	config(
+		target_database = "DEV_DB",
+		target_schema = "DBT_FPETRIBUFUNDTHROUGHCOM",
+		strategy='check',
+		unique_key='product_id',
+		check_cols=['inventory'],
+	)
+}}
+
+SELECT 
+  product_id,
+  name, 
+  price,
+  inventory
+FROM {{ source("postgres", "products") }}
+
+{% endsnapshot %}

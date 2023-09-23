fc2613733d0c146d8f9735e6ce7bd30592ebbd5a
Amelia Helland <amelia@sigmacomputing.com>
2023-09-23 20:51:29 +0000

    Define query for promo profitability.

---
+++ greenery/models/marts/marketing/promo_profitability.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+SELECT
+    p.promo_id,
+    p.discount,
+    COUNT(DISTINCT o.order_id) AS total_orders,
+    SUM(order_total) AS total_revenue,
+    (SUM(order_total) - (COUNT(DISTINCT o.order_id) * p.discount)) AS campaign_profit
+FROM
+    {{ ref('postgres.stg_postgres__promos') }} AS p
+LEFT JOIN
+    {{ ref('postgres.stg_postgres__orders') }} AS o
+ON
+    p.promo_id = o.promo_id
+GROUP BY
+    p.promo_id, p.discount

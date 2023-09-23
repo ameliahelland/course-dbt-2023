df4c9d83798b339bbdce4f9b1d72fad751a41291
Amelia Helland <amelia@sigmacomputing.com>
2023-09-23 22:00:31 +0000

    Make column names more consistent.

--- greenery/models/marts/core/customers_and_churn_status.sql
+++ greenery/models/marts/core/customers_and_churn_status.sql
@@ -13,7 +13,7 @@ SELECT
     CASE
         WHEN last_order_date IS NULL OR last_order_date < TO_DATE('2021-02-11') - INTERVAL '90 days' THEN 'Churned'
         ELSE 'Active'
-    END AS churn_status
+    END AS ChurnStatus
 FROM
     {{ ref('postgres.stg_postgres__users') }} AS u
 LEFT JOIN (

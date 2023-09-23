87314554fa832fad88bf403cf95990172e1a8e92
Amelia Helland <amelia@sigmacomputing.com>
2023-09-23 22:27:21 +0000

    Fix source references.

--- greenery/models/marts/core/session_events_and_user_orders.sql
+++ greenery/models/marts/core/session_events_and_user_orders.sql
@@ -22,10 +22,10 @@ SELECT
     o.estimated_delivery_at AS EstimatedDeliveryAt,
     o.delivered_at AS DeliveredAt
 FROM
-    {{ ref('postgres.stg_postgres__events') }} e
+    {{ source('postgres', 'stg_postgres__events') }} e
 LEFT JOIN
-    {{ ref('postgres.stg_postgres__users') }} u ON e.user_id = u.user_id
+    {{ source('postgres', 'stg_postgres__users') }} u ON e.user_id = u.user_id
 LEFT JOIN
-    {{ ref('postgres.stg_postgres__orders') }} o ON e.order_id = o.order_id
+    {{ source('postgres', 'stg_postgres__orders') }} o ON e.order_id = o.order_id
 ORDER BY
     e.session_id, e.created_at

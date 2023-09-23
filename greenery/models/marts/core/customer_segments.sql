b80b003e5e9835c852b0bda141600795b06c9416
Amelia Helland <amelia@sigmacomputing.com>
2023-09-23 22:08:30 +0000

    Make column names consistent.

--- greenery/models/marts/core/customer_segments.sql
+++ greenery/models/marts/core/customer_segments.sql
@@ -5,14 +5,14 @@
 }}
 
 SELECT
-    u.user_id,
-    u.first_name,
-    u.last_name,
+    u.user_id AS UserId,
+    u.first_name AS FirstName,
+    u.last_name AS LastName,
     CASE
         WHEN COUNT(o.order_id) >= 5 THEN 'High-Value'
         WHEN COUNT(o.order_id) >= 2 THEN 'Regular'
         ELSE 'Infrequent'
-    END AS customer_segment,
+    END AS CustomerSegment,
     COUNT(o.order_id) as total_orders
 FROM
     {{ ref('postgres.stg_postgres__users') }} AS u

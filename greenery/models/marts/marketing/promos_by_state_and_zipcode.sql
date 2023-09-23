505c27d0ad3efe0f4f7b6d6394920d6183b2ace3
Amelia Helland <amelia@sigmacomputing.com>
2023-09-23 20:51:48 +0000

    Define query for promos by state and zipcode.

---
+++ greenery/models/marts/marketing/promos_by_state_and_zipcode.sql
@@ -0,0 +1,35 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+WITH PromoCounts AS (
+    SELECT
+        a.state,
+        a.zipcode,
+        COUNT(DISTINCT o.order_id) AS total_orders,
+        COUNT(DISTINCT CASE WHEN o.promo_id IS NOT NULL THEN o.order_id END) AS promo_orders
+    FROM
+        {{ ref('postgres.stg_postgres__orders') }} AS o
+    JOIN
+        {{ ref('postgres.stg_postgres__addresses') }} AS a
+    ON
+        o.address_id = a.address_id
+    GROUP BY
+        a.state,
+        a.zipcode
+)
+SELECT
+    pc.state,
+    pc.zipcode,
+    pc.total_orders,
+    pc.promo_orders,
+    CASE
+        WHEN pc.total_orders > 0 THEN Round((pc.promo_orders::FLOAT / pc.total_orders) * 100, 2)
+        ELSE 0
+    END AS promo_application_rate
+FROM
+    PromoCounts AS pc
+ORDER BY
+    promo_application_rate DESC

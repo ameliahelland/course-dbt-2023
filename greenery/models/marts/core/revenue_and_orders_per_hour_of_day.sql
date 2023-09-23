897a67bdc6ce8fd9cbca5e39f25b363fb71cd5e8
Amelia Helland <amelia@sigmacomputing.com>
2023-09-23 21:01:19 +0000

    Define query for revenue and orders by hour of day.

---
+++ greenery/models/marts/core/revenue_and_orders_per_hour_of_day.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+SELECT
+    TRUNC(TO_DATE(created_at), 'DAY') AS order_day,
+    DATE_PART(HOUR, created_at) AS order_hour,
+    SUM(order_total) AS total_revenue,
+    ROUND(AVG(order_total), 2) AS average_order_total,
+    COUNT(order_id) AS orders_per_hour
+FROM
+    {{ ref('postgres.stg_postgres__orders') }}
+GROUP BY
+    order_day, order_hour
+ORDER BY
+    order_day, order_hour

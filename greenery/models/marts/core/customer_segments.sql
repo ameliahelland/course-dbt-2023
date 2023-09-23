0c4b88c5147be7a8f9d888dc7cc0920d070c4f18
Amelia Helland <amelia@sigmacomputing.com>
2023-09-23 21:01:42 +0000

    Define query to list customers with their customer segment.

---
+++ greenery/models/marts/core/customer_segments.sql
@@ -0,0 +1,24 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+SELECT
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    CASE
+        WHEN COUNT(o.order_id) >= 5 THEN 'High-Value'
+        WHEN COUNT(o.order_id) >= 2 THEN 'Regular'
+        ELSE 'Infrequent'
+    END AS customer_segment,
+    COUNT(o.order_id) as total_orders
+FROM
+    {{ ref('postgres.stg_postgres__users') }} AS u
+LEFT JOIN
+    {{ ref('postgres.stg_postgres__orders') }} AS o
+ON
+    u.user_id = o.user_id
+GROUP BY
+    u.user_id, u.first_name, u.last_name

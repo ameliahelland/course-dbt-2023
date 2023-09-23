9248f336ef826fe35fd22ed320cfe35103711a97
Amelia Helland <amelia@sigmacomputing.com>
2023-09-23 22:28:10 +0000

    Fix source references.

--- greenery/models/marts/core/revenue_and_orders_per_hour_of_day.sql
+++ greenery/models/marts/core/revenue_and_orders_per_hour_of_day.sql
@@ -11,7 +11,7 @@ SELECT
     ROUND(AVG(order_total), 2) AS AverageOrderTotal,
     COUNT(order_id) AS OrdersPerHour
 FROM
-    {{ ref('postgres.stg_postgres__orders') }}
+    {{ source('postgres', 'stg_postgres__orders') }}
 GROUP BY
     OrderDay, OrderHour
 ORDER BY

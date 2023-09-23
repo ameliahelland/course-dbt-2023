4fef961f3441c34f42119530fab769593cb20ab8
Amelia Helland <amelia@sigmacomputing.com>
2023-09-23 21:33:22 +0000

    Make column names consistent.

--- greenery/models/marts/marketing/promo_profitability.sql
+++ greenery/models/marts/marketing/promo_profitability.sql
@@ -5,11 +5,11 @@
 }}
 
 SELECT
-    p.promo_id,
+    p.promo_id as PromoId,
     p.discount,
-    COUNT(DISTINCT o.order_id) AS total_orders,
+    COUNT(DISTINCT o.order_id) AS TotalOrders,
     SUM(order_total) AS total_revenue,
-    (SUM(order_total) - (COUNT(DISTINCT o.order_id) * p.discount)) AS campaign_profit
+    (SUM(order_total) - (COUNT(DISTINCT o.order_id) * p.discount)) AS CampaignProfit
 FROM
     {{ ref('postgres.stg_postgres__promos') }} AS p
 LEFT JOIN

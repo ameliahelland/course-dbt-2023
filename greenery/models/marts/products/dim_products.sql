31ab7a92c443d503c07cec5d1c1d3afe166fa0be
Amelia Helland <amelia@sigmacomputing.com>
2023-09-26 22:24:25 +0000

    Add new columns from the related_products model.

--- greenery/models/marts/products/dim_products.sql
+++ greenery/models/marts/products/dim_products.sql
@@ -10,14 +10,29 @@ SELECT
     Inventory,
     AverageAmountPurchasedPerOrder,
     PageViews,
-    Revenue
+    Revenue,
+    MostPopularItem1,
+    FrequencyItem1,
+    MostPopularItem2,
+    FrequencyItem2,
+    MostPopularItem3,
+    FrequencyItem3,
+    MostPopularItem4,
+    FrequencyItem4,
+    MostPopularItem5,
+    FrequencyItem5
+
 FROM
-    {{ ref('int__product_inventory_and_order_statistics') }} pios
+    {{ ref('int__product_inventory_and_order_statistics') }} AS pios
 LEFT JOIN
-    {{ ref('int__product_pageviews') }} pp
+    {{ ref('int__product_pageviews') }} AS pp
 ON
     pios.ProductName = pp.ProductName
 LEFT JOIN
-    {{ ref('int__product_revenues') }} pr
+    {{ ref('int__product_revenues') }} AS pr
 ON
     pios.ProductName = pr.ProductName
+LEFT JOIN
+    {{ ref('int__related_products') }} AS rp
+ON
+    pios.ProductName = rp.ProductName

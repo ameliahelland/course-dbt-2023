9aa030aaa46a631725eb6581aed821508b4e8738
Amelia Helland <amelia@sigmacomputing.com>
2023-09-26 23:57:48 +0000

    Revise model to include promo IDs.

--- greenery/models/marts/marketing/promos_user_session_events.sql
+++ greenery/models/marts/marketing/promos_user_session_events.sql
@@ -4,34 +4,20 @@
     )
 }}
 
-WITH PromoSessionEvents AS (
-    SELECT
-        e.user_id,
-        e.session_id,
-        e.event_id,
-        e.created_at AS event_created_at,
-        o.order_id,
-        o.created_at AS order_created_at,
-        o.promo_id
-    FROM
-        {{ ref('stg_postgres__events') }} AS e
-    LEFT JOIN
-        {{ ref('stg_postgres__orders') }} AS o
-    ON
-        e.order_id = o.order_id
-    WHERE
-        o.promo_id IS NOT NULL
-)
+
 SELECT
-    pse.user_id AS UserId,
-    pse.session_id AS SessionId,
-    pse.event_id AS EventId,
-    pse.event_created_at AS EventCreatedAt,
-    pse.order_id AS OrderId,
-    pse.order_created_at AS OrderCreatedAt,
-    CASE
-        WHEN pse.order_id IS NOT NULL AND pse.promo_id IS NOT NULL THEN 1
-        ELSE 0
-    END AS promo_order_flag
+    e.user_id,
+    e.session_id,
+    e.event_id,
+    e.created_at AS event_created_at,
+    o.order_id,
+    o.created_at AS order_created_at,
+    o.promo_id
 FROM
-    PromoSessionEvents AS pse
+    {{ ref('stg_postgres__events') }} AS e
+LEFT JOIN
+    {{ ref('stg_postgres__orders') }} AS o
+ON
+    e.order_id = o.order_id
+WHERE
+    o.promo_id IS NOT NULL

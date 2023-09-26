ea642bf8350193dada9ee94d3c88437b244b67f2
Amelia Helland <amelia@sigmacomputing.com>
2023-09-26 18:34:42 +0000

    Replace source with reference to staging models.

--- greenery/models/marts/products/product_pageviews.sql
+++ greenery/models/marts/products/product_pageviews.sql
@@ -10,9 +10,9 @@ WITH ProductPageViews AS (
         p.name AS ProductName,
         COUNT(*) AS PageViews
     FROM
-        {{ source('stg_postgres__products') }} p
+        {{ ref('stg_postgres__products') }} p
     JOIN
-        {{ source('stg_postgres__events') }} e ON p.product_id = e.product_id
+        {{ ref('stg_postgres__events') }} e ON p.product_id = e.product_id
     WHERE
         e.event_type = 'page_view'
     GROUP BY

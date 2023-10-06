b9d9d04c23a6300c6867bdaf5aea9dbb98652ca1
Amelia Helland <amelia@sigmacomputing.com>
2023-10-06 18:41:09 +0000

    Add orders dimension model for funnel analysis.

---
+++ greenery/models/marts/core/dim_orders.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+SELECT
+    e.session_id,
+    MAX(CASE WHEN e.event_type = 'add_to_cart' THEN e.product_id END) AS product_id,
+    MAX(CASE WHEN e.event_type = 'add_to_cart' THEN e.event_id END) AS cart_event_id,
+    MAX(CASE WHEN e.event_type = 'checkout' THEN e.event_id END) AS checkout_event_id,
+    MAX(CASE WHEN e.event_type = 'checkout' THEN e.order_id END) AS order_id
+FROM
+    {{ source('stg_postgres__events') }} e
+WHERE
+    e.event_type IN ('add_to_cart', 'checkout')
+GROUP BY
+    e.session_id

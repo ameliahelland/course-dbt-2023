283425ba69bb6ffc41b6b5bb0021a4acb8503611
Amelia Helland <amelia@sigmacomputing.com>
2023-10-06 18:49:53 +0000

    Fix reference to events table.

--- greenery/models/marts/core/dim_orders.sql
+++ greenery/models/marts/core/dim_orders.sql
@@ -11,7 +11,7 @@ SELECT
     MAX(CASE WHEN e.event_type = 'checkout' THEN e.event_id END) AS checkout_event_id,
     MAX(CASE WHEN e.event_type = 'checkout' THEN e.order_id END) AS order_id
 FROM
-    {{ source('stg_postgres__events') }} e
+    {{ ref('stg_postgres__events') }} e
 WHERE
     e.event_type IN ('add_to_cart', 'checkout')
 GROUP BY

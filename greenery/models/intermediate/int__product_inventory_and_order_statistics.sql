96a81fff478132981d4b65a70f1ee69355d2e561
Amelia Helland <amelia@sigmacomputing.com>
2023-10-09 00:06:21 +0000

    Update column names and definitions.

--- greenery/models/intermediate/int__product_inventory_and_order_statistics.sql
+++ greenery/models/intermediate/int__product_inventory_and_order_statistics.sql
@@ -8,11 +8,12 @@ WITH product_inventory AS (
     SELECT
         p.name AS product_name,
         p.product_id,
-        SUM(oi.quantity) AS inventory_used,
         p.inventory,
-        COUNT(DISTINCT oi.order_id) AS total_orders
+        SUM(oi.quantity) AS inventory_used,
+        COUNT(DISTINCT oi.order_id) AS total_orders,
+        SUM(oi.quantity * p.price) AS revenue
     FROM {{ ref('stg_postgres__order_items') }} oi
-    JOIN {{ ref('stg_postgres__products') }} p
+    LEFT JOIN {{ ref('stg_postgres__products') }} p
     ON oi.product_id = p.product_id
     GROUP BY ALL
 )
@@ -20,7 +21,8 @@ SELECT
     product_name,
     product_id,
     inventory_used,
-    inventory,
+    inventory AS inventory_remaining,
+    revenue,
     ROUND(inventory_used / total_orders, 2) AS average_amount_purchased_per_order
 FROM product_inventory
-ORDER BY inventory - inventory_used ASC
+ORDER BY inventory_remaining - inventory_used ASC
